932fb4f7fc6ddf3b98e74f19fa8b39693ea258f8
vaz-git <gabriel.v.jesus7@aluno.senai.br>
2026-02-26 16:19:12 -0300

    Desafio 5 A e B completo

--- js-base-(alunos)/ex1-manipulacao-dom/script.js
+++ js-base-(alunos)/ex1-manipulacao-dom/script.js
@@ -1,8 +1,12 @@
 // Seleciona o elemento do botão no DOM usando o ID "myButton" e armazena na variável 'button'
 
-// 3ª Digitação (Aqui) 
+// 3ª Digitação (Aqui)  
+const button = document.querySelector("#myButton");
 
 // Seleciona o elemento de parágrafo no DOM usando o ID "myText" e armazena na variável 'text'
-
+const text= document.querySelector("#myText");
 
 // Adiciona um "ouvinte" de evento ao botão. Quando o botão for clicado, a função dentro do 'addEventListener' será executada
+button.addEventListener('click', () => {
+    text.textContent = 'Olá Mundo!';
+})
